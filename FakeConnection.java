0db3151d5174adf2df941b38baaf4368ab2a387e
Tedval88 <teddyche@hotmail.fr>
2013-12-07 15:36:53 +0100

    Update FakeConnection.java

--- FakeConnection.java
+++ FakeConnection.java
@@ -1,5 +1,7 @@
 package com.aurigaming.fakeconnection;
 
+import java.io.File;
+
 import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
@@ -10,9 +12,13 @@ public class FakeConnection extends JavaPlugin {
 	
 	@Override
 	public void onEnable() {
-		System.out.println("FakeConnection charge avec succes !");
+	System.out.println("FakeConnection charge avec succes !");
+	File config = new File(getDataFolder(), "config.yml");
+	if(!config.exists()) {
+		this.saveDefaultConfig();
 	}
-	
+	}
+
 	@Override
 	public boolean onCommand(final CommandSender sender, Command cmd, String label,
 	String[] args) {
@@ -24,13 +30,13 @@ public class FakeConnection extends JavaPlugin {
 	return false;
 	}
 	if(cmd.getName().equalsIgnoreCase("freco")) {
-		this.getServer().broadcastMessage(ChatColor.YELLOW + player.getName() + " joined the game.");
+		this.getServer().broadcastMessage(ChatColor.YELLOW + player.getName() + " " + this.getConfig().getString("Messages.Co"));
 	}
 	if(cmd.getName().equalsIgnoreCase("fdeco")) {
-		this.getServer().broadcastMessage(ChatColor.YELLOW + player.getName() + " left the game.");
+		this.getServer().broadcastMessage(ChatColor.YELLOW + player.getName() + " " + this.getConfig().getString("Messages.Deco"));
 	}
 	if(cmd.getName().equalsIgnoreCase("fafk")) {
-		this.getServer().broadcastMessage(ChatColor.WHITE + player.getDisplayName() + " est désormais AFK");
+		this.getServer().broadcastMessage(ChatColor.WHITE + player.getDisplayName() + " " + this.getConfig().getString("Messages.AFK"));
 	}
 	return true;
 	}
