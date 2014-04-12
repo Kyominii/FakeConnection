9867b90090ea667dab240a0b9b892a31290ad149
Tedval88 <teddyche@hotmail.fr>
2014-04-12 14:18:14 +0200

    Update FakeConnection.java

--- src/com/aurigaming/fakeconnection/FakeConnection.java
+++ src/com/aurigaming/fakeconnection/FakeConnection.java
@@ -1,6 +1,7 @@
 package com.aurigaming.fakeconnection;
 
 import java.io.File;
+import java.io.IOException;
 
 import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
@@ -8,15 +9,31 @@ import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import com.comphenix.protocol.ProtocolLibrary;
+import com.comphenix.protocol.ProtocolManager;
+
 public class FakeConnection extends JavaPlugin {
+	
+	private ProtocolManager protocolManager;
+    private HidePlayerList hidePlayer;
 	
 	@Override
 	public void onEnable() {
-	System.out.println("FakeConnection charge avec succes !");
+	hidePlayer = new HidePlayerList(this);
+	protocolManager = ProtocolLibrary.getProtocolManager();
 	File config = new File(getDataFolder(), "config.yml");
 	if(!config.exists()) {
 		this.saveDefaultConfig();
 	}
+	if(this.getConfig().getString("Others.mcstats")=="true") {
+		try {
+			Metrics metrics = new Metrics(this);
+			metrics.start();
+		} catch (IOException e) {
+			// Failed to submit the stats :-(
+		}
+	}
+	System.out.println("FakeConnection charge avec succes !");
 	}
 
 	@Override
@@ -31,12 +48,25 @@ public class FakeConnection extends JavaPlugin {
 	}
 	if(cmd.getName().equalsIgnoreCase("freco")) {
 		this.getServer().broadcastMessage(ChatColor.YELLOW + player.getName() + " " + this.getConfig().getString("Messages.Co"));
+		hidePlayer.showPlayer(player);
+		for(Player p : getServer().getOnlinePlayers()) {
+			player.showPlayer(player);
+		}
+		player.sendMessage(ChatColor.GRAY + "[" + ChatColor.GOLD + "FakeConnection" + ChatColor.GRAY + "] " + ChatColor.GREEN + "Hello :D");
 	}
 	if(cmd.getName().equalsIgnoreCase("fdeco")) {
 		this.getServer().broadcastMessage(ChatColor.YELLOW + player.getName() + " " + this.getConfig().getString("Messages.Deco"));
+		hidePlayer.hidePlayer(player);
+		for(Player p : getServer().getOnlinePlayers()) {
+			player.hidePlayer(player);
+		}
+		player.sendMessage(ChatColor.GRAY + "[" + ChatColor.GOLD + "FakeConnection" + ChatColor.GRAY + "] " + ChatColor.GREEN + "Bye bye ;)");
 	}
 	if(cmd.getName().equalsIgnoreCase("fafk")) {
 		this.getServer().broadcastMessage(ChatColor.WHITE + player.getDisplayName() + " " + this.getConfig().getString("Messages.AFK"));
+	}
+	if(cmd.getName().equalsIgnoreCase("fnafk")) {
+		this.getServer().broadcastMessage(ChatColor.WHITE + player.getDisplayName() + " " + this.getConfig().getString("Messages.nAFK"));
 	}
 	return true;
 	}
